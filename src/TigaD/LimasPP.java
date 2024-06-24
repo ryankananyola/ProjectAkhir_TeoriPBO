67558eea95f4fe84d8006e9b6a047febec0b48b0
ryankananyola <mariadestan2027@gmail.com>
2024-06-24 17:53:03 +0700

    TigaD fix

--- src/TigaD/LimasPP.java
+++ src/TigaD/LimasPP.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TigaD;
 
 import DuaD.PersegiPanjang;
 
-/**
- *
- * @author USER
- */
 public class LimasPP extends PersegiPanjang {
     
     public double tinggiLimasPP;
--- src/TigaD/LimasPersegi.java
+++ src/TigaD/LimasPersegi.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TigaD;
 
 import DuaD.Persegi;
 
-
-/**
- *
- * @author USER
- */
 public class LimasPersegi extends Persegi{    
     
     public double tinggiLimas;
